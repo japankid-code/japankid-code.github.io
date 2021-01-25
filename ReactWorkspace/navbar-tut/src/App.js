6640764ab70335a0fba4c0b0c72f65328d885131
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-24 22:01:32 -0600

    update gitignore, add imports to app.js

--- ReactWorkspace/navbar-tut/src/App.js
+++ ReactWorkspace/navbar-tut/src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
