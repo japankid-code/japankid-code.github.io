bc9c3d2d2d2193f859183c42e260da693a491793
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-25 20:59:21 -0600

    fix the css thing, its backticks not apostrophes!!

--- ReactWorkspace/navbar-tut/src/components/Sidebar.js
+++ ReactWorkspace/navbar-tut/src/components/Sidebar.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import from "styled-components";
 
-    /* This defines the actual bar going down the screen */
-const StyledSideNav = styled.div'
-    position: fixed;    /* Fixed Sidebar (stay in place on scroll and position relative to viewport) */
-    height: 100%;
-    width: 75px;        /* Set the width of the sidebar */
-    z-index: 1;         /* Stay on top of everything */
-    top: 3.4em;         /* Stay at the top */
-    background-color: #222; /* Black */
-    overflow-x: hidden;     /* Disable horizontal scroll */
-    padding-top: 10px;
-';
+/* This defines the actual bar going down the screen */
+const StyledSideNav = styled.div`
+  position: fixed;     /* Fixed Sidebar (stay in place on scroll and position relative to viewport) */
+  height: 100%;
+  width: 75px;     /* Set the width of the sidebar */
+  z-index: 1;      /* Stay on top of everything */
+  top: 3.4em;      /* Stay at the top */
+  background-color: #222; /* Black */
+  overflow-x: hidden;     /* Disable horizontal scroll */
+  padding-top: 10px;
+`;
 
 
 class SideNav extends React.Component {
