e93f0bcb0bb65b7fe7d7063e209fe09d7e97091c
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-27 19:57:59 -0600

    remove some unneeded imports

--- ReactWorkspace/navbar-tut/src/App.js
+++ ReactWorkspace/navbar-tut/src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
