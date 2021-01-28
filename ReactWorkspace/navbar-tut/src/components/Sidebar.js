f3a0ac0caf03b9406b983caffdfc0c57790abfe0
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-27 20:36:25 -0600

    add nav item constants

--- ReactWorkspace/navbar-tut/src/components/Sidebar.js
+++ ReactWorkspace/navbar-tut/src/components/Sidebar.js
@@ -17,6 +17,21 @@ const NavIcon = styled.div `
 
 `;
 
+const StyledNavItem = styled.div`
+  height: 70px;
+  width: 75px; /* width must be same size as NavBar to center */
+  text-align: center; /* Aligns <a> inside of NavIcon div */
+  margin-bottom: 0;   /* Puts space between NavItems */
+  a {
+    font-size: 2.7em;
+    color: ${(props) => props.active ? "white" : "#9FFFCB"};
+    :hover {
+      opacity: 0.7;
+      text-decoration: none; /* Gets rid of underlining of icons */
+    }  
+  }
+`;
+
 class SideNav extends React.Component {
     render() {
       return(
