c779a70ee1c1664ab946907c11e2f54b929caac1
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-31 21:55:23 -0600

    add themeToggler.js. update style.css media tags to handle changing themes
    source for themeToggler.js: https://kevdees.com/adding-dark-mode-toggle-to-your-website
    heavily edited to get it working on my end :)
    not quite working yet tho :thought_balloon:

---
+++ ReactWorkspace/todo/public/themeToggler.js
@@ -0,0 +1,28 @@
+// this was gotten here: https://kevdees.com/adding-dark-mode-toggle-to-your-website/#:~:text=To%20add%20a%20dark%20and,after%20the%20opening%20body%20tag.
+// A function for toggling the theme
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
+
+    localStorage.setItem(
+        "colorTheme", // detects what color theme is set to.
+        document.body.classList.contains("dark") ? "dark" : "light" // sets the theme.
+    );
+    // console.log(localStorage.getItem("colorTheme"))
+}
+
+// Initialize the theme
+function themeInitializer() {
+    if (localStorage.getItem("colorTheme") === "light" &&
+        window.matchMedia("(prefers-color-scheme: light)").matches
+        ) {
+            toggleTheme();
+    } else if (
+        localStorage.getItem("colorTheme") === "dark" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        toggleTheme();
+    }
+}
+
+themeInitializer();
